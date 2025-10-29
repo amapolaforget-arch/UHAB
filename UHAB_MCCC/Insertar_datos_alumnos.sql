efa03fc63962277a22fe4e0c5ba5ca285e28bf7f
amapolaforget-arch <amapolaforget@gmail.com>
2025-10-28 22:33:12 -0600

    SE AGREGA CARPETA DE EXAMEN
    HOLI! SE ADJUNTA ARCHIVO DEL EXAMEN DEL DIA 25/10/2025

---
+++ UHAB_MCCC/Insertar_datos_alumnos.sql
@@ -0,0 +1,22 @@
+
+INSERT INTO UHAB.Alumnos (`´Id_Alumno´`, `´Matricula´`, `´Nombre´`, `´Apellidos´`, `´Email´`) VALUES 
+(NULL, '1256', 'MARY CARMEN', 'CRUZ', 'marycruz@gmail.com'),
+(NULL, '12347', 'REGINA', 'CRUZ', 'regina@gmail.com'),
+(NULL, '12345', 'JOSE', 'CRUZ', 'josefo@gmail.com'),
+(NULL, '12347', 'ARTURO', 'CRUZ', 'arturitou@gmail.com'),
+(NULL, '12348', 'LUCIO', 'CRUZ', 'eloso@gmail.com');
+
+INSERT INTO UHAB.Clases (`´Id_Clase´`, `´Nombre_Clase´`, `´Nombre_Profesor´`) VALUES 
+(NULL, 'Base de Datos II', 'Jose Luis Hernandez'),
+(NULL, 'Arquitectura de Redes', 'Carlos Martinez'),
+(NULL, 'Programacion Web', 'Cristian Cruz'),
+(NULL, 'Ingenieria Economica', 'Jose Luis Aguilar'),
+(NULL, 'Arquitectura de computadoras', 'Manuel Lopez');
+
+INSERT INTO UHAB.Notas (`´Id_Nota´`, `´Califiacion´`) VALUES 
+(NULL, '8.5'),
+(NULL, '7.5'),
+(NULL, '8.3'),
+(NULL, '6.5'),
+(NULL, '9.1');
+
